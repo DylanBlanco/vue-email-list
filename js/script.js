87fd757346e546cb91fe6d284ed89b06d21be360
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-03 10:25:56 +0200

    generato e stampato ciclo di 10 mail

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 const {createApp} = Vue;
 
-createApp(
+const app = createApp(
     {
         data() {
             return {
-                message: 'Hello World',
-                image: 'img/city-dark.jpeg'
+                emails: []
             };
+        },
+        created() {
+            for(let i=0; i<10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        const email = res.data.response;
+                        console.log('email' + i + ':', email);
+    
+                        this.emails.push(email);
+                    });
+            }
         }
     }
 ).mount('#app');
